d78ab79133231bdca9e8a3bd648fe8f760ac4016
glooneltharion <gloo.neltharion@gmail.com>
2021-11-06 14:16:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var first = $('.first'),
     second = $('.second'),
     last = $('.last'),
     btn = $('button'),
+    color = $('#input').val(),
     style = {
-        background: "yellow" 
+        background: color 
     };
+
+console.log(color);
 
 first.on('click', () => {
         console.log("Yeah, you clicked me' to the console when the user clicks on the button")
